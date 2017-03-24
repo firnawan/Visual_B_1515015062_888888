655ef4fefbbeca8c9c696361f6c2317ec7aa81f7
firnawan <firnawanh@gmail.com>
2017-03-24 14:00:57 +0800

    tugas keempat

---
+++ Visual_B_1515015062_888888/src/visual_b_1515015062_888888/Visual_B_1515015062_888888.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visual_b_1515015062_888888;
+
+/**
+ *
+ * @author Awan
+ */
+public class Visual_B_1515015062_888888 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
